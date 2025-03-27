d9bad2fb24b821730900be52eb72639d5f711e96
Chitti Rupas <chittirupasmaguluri@gmail.com>
2025-03-27 18:50:55 +0530

    first-commit

---
+++ src/main/java/com/inventorymanagement/entity/Inventory.java
@@ -0,0 +1,20 @@
+package com.inventorymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private int quantity;
+}
+
